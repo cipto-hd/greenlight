4f53cc30cd6bd63bd13bb44937afb2105854bf3b
Cipto Hadi <ciptohadi79@gmail.com>
2024-02-07 23:49:58 +0700

    extend optimistic locking with round-trip locking

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/cipto-hd/greenlight/internal/data"
 	"github.com/cipto-hd/greenlight/internal/validator"
@@ -115,6 +116,15 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 			app.serverErrorResponse(w, r, err)
 		}
 		return
+	}
+
+	// If the request contains a X-Expected-Version header, verify that the movie
+	// version in the database matches the expected version specified in the header.
+	if r.Header.Get("X-Expected-Version") != "" {
+		if strconv.FormatInt(int64(movie.Version), 32) != r.Header.Get("X-Expected-Version") {
+			app.editConflictResponse(w, r)
+			return
+		}
 	}
 
 	// Declare an input struct to hold the expected data from the client.
