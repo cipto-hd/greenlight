0cb3c6df83aca660f4a8c86e47879cc0a57bb63e
Cipto Hadi <ciptohadi79@gmail.com>
2024-02-11 06:31:19 +0700

    intercepting shutdown signals SIGINT & SIGTERM

---
+++ cmd/api/server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/cipto-hd/greenlight/internal/jsonlog"
+)
+
+func (app *application) serve(cfg config, logger *jsonlog.Logger) error {
+	// Start a background goroutine.
+	go func() {
+		// Create a quit channel which carries os.Signal values.
+		quit := make(chan os.Signal, 1)
+		// Use signal.Notify() to listen for incoming SIGINT and SIGTERM signals and
+		// relay them to the quit channel. Any other signals will not be caught by
+		// signal.Notify() and will retain their default behavior.
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		// Read the signal from the quit channel. This code will block until a signal is
+		// received.
+		s := <-quit
+		// Log a message to say that the signal has been caught. Notice that we also
+		// call the String() method on the signal to get the signal name and include it
+		// in the log entry properties.
+		app.logger.PrintInfo("caught signal", map[string]string{
+			"signal": s.String(),
+		})
+		// Exit the application with a 0 (success) status code.
+		os.Exit(0)
+	}()
+
+	// Declare a HTTP server with some sensible timeout settings, which listens on the
+	// port provided in the config struct and uses the servemux we created above as the
+	// handler.
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.port),
+		Handler: app.routes(),
+		// Create a new Go log.Logger instance with the log.New() function, passing in
+		// our custom Logger as the first parameter. The "" and 0 indicate that the
+		// log.Logger instance should not use a prefix or any flags.
+		ErrorLog:     log.New(logger, "", 0),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	// Again, we use the PrintInfo() method to write a "starting server" message at the
+	// INFO level. But this time we pass a map containing additional properties (the
+	// operating environment and server address) as the final parameter.
+	logger.PrintInfo("starting server", map[string]string{
+		"addr": srv.Addr,
+		"env":  cfg.env,
+	})
+
+	return srv.ListenAndServe()
+}
