e6545f30e8762103c9fa74181d7ac93adb071d16
Cipto Hadi <ciptohadi79@gmail.com>
2024-02-12 07:05:55 +0700

    make graceful shutdown waiting for background tasks to complete using sync.WaitGroup

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -34,7 +34,7 @@ func (app *application) serve(cfg config, logger *jsonlog.Logger) error {
 	// by the graceful Shutdown() function.
 	shutdownError := make(chan error)
 
-	// Start a background goroutine.
+	// Start a background goroutine to listen for incoming SIGINT and SIGTERM signals.
 	go func() {
 		// Create a quit channel which carries os.Signal values.
 		quit := make(chan os.Signal, 1)
@@ -60,12 +60,23 @@ func (app *application) serve(cfg config, logger *jsonlog.Logger) error {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 
-		// Call Shutdown() on our server, passing in the context we just made.
-		// Shutdown() will return nil if the graceful shutdown was successful, or an
-		// error (which may happen because of a problem closing the listeners, or
-		// because the shutdown didn't complete before the 5-second context deadline is
-		// hit). We relay this return value to the shutdownError channel.
-		shutdownError <- srv.Shutdown(ctx)
+		// Call Shutdown() on the server like before, but now we only send on the
+		// shutdownError channel if it returns an error.
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			shutdownError <- err
+		}
+		// Log a message to say that we're waiting for any background goroutines to
+		// complete their tasks.
+		app.logger.PrintInfo("completing background tasks", map[string]string{
+			"addr": srv.Addr,
+		})
+		// Call Wait() to block until our WaitGroup counter is zero --- essentially
+		// blocking until the background goroutines have finished. Then we return nil on
+		// the shutdownError channel, to indicate that the shutdown completed without
+		// any issues.
+		app.wg.Wait()
+		shutdownError <- nil
 	}()
 
 	// Again, we use the PrintInfo() method to write a "starting server" message at the
